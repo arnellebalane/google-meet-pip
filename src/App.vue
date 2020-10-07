7fd291efa2bae45f95361eddfd9c101a59ab054d
Arnelle Balane <arnellebalane@gmail.com>
2020-10-08 02:30:01 +0800

    Improve styling for popup screen UI

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,25 @@
   <div v-if="state === STATE.LOADING" class="loading-screen"></div>
 
   <div v-if="state === STATE.SELECTION" class="selection-screen">
+    <p>Choose the participant to place in Picture-in-Picture window</p>
+    <small>Only the participants that are visible on your screen can be selected</small>
+
     <ul>
-      <li v-for="participant in participants" :key="participant.name">
+      <li v-for="participant in participants" :key="participant.name" :class="{ disabled: !participant.active }">
         {{ participant.name }}
       </li>
     </ul>
   </div>
 
   <div v-if="state === STATE.ERROR" class="error-screen">
-    <h1>Error!</h1>
+    <svg width="40" height="40" viewBox="0 0 24 24">
+      <path
+        fill="#c84031"
+        d="M3.41,1.86L2,3.27L4.73,6H4A1,1 0 0,0 3,7V17A1,1 0 0,0 4,18H16C16.21,18 16.39,17.92 16.55,17.82L19.73,21L21.14,19.59L12.28,10.73L3.41,1.86M5,16V8H6.73L14.73,16H5M15,8V10.61L21,16.61V6.5L17,10.5V7A1,1 0 0,0 16,6H10.39L12.39,8H15Z"
+      />
+    </svg>
+    <p>Unable to get meeting participants</p>
+    <p>Please make sure that you have joined the Google Meet meeting and try again</p>
   </div>
 </template>
 
