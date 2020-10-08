a818e0a242c97b59183546ce231a24059f65569b
Arnelle Balane <arnellebalane@gmail.com>
2020-10-09 00:30:20 +0800

    Handle long participant names with ellipsis text overflow

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,21 @@
     <ul>
       <li
         v-for="participant in participants"
-        :key="participant.name"
+        :key="participant.id"
         :class="{ disabled: !participant.available, active: participant.active }"
         @click="handleParticipantClick(participant)"
       >
-        {{ participant.name }}
+        <span class="name">{{ participant.name }}</span>
 
-        <template v-if="participant.active">
+        <div v-if="participant.active" class="info">
           <svg width="16" height="16" viewBox="0 0 24 24">
             <path
               fill="#00796b"
               d="M19,11H11V17H19V11M23,19V5C23,3.88 22.1,3 21,3H3A2,2 0 0,0 1,5V19A2,2 0 0,0 3,21H21A2,2 0 0,0 23,19M21,19H3V4.97H21V19Z"
             />
           </svg>
-          <span>click to exit</span>
-        </template>
+          <span class="exit">click to exit</span>
+        </div>
       </li>
     </ul>
   </div>
